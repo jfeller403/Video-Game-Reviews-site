a83e55a1465cb742b1b564216e2b903219ce8277
Jesse Feller <51204244+jfeller403@users.noreply.github.com>
2021-10-28 21:00:38 -0400

    Initialize Averages

---
+++ ReviewsSite/Controllers/VideoGameController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReviewsSite.Models;
+using ReviewsSite.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Controllers
+{
+    public class VideoGameController : Controller
+    {
+        public VideoGameRepository videoGameRepo;
+        public VideoGameContext db;
+        public VideoGameController(VideoGameRepository videoGameRepo, VideoGameContext db)
+        {
+            this.videoGameRepo = videoGameRepo;
+            this.db = db;
+        }
+        public ViewResult Index()
+        {
+            var videoGames = db.VideoGames.ToList();
+
+            foreach(var game in videoGames)
+            {
+                game.GetAverage();
+            }
+
+            return View(videoGames);
+        }
+        public ViewResult Details(int id)
+        {
+            return View(videoGameRepo.GetByID(id));
+        }
+        public IActionResult Create()
+        {
+            return View(new VideoGame());
+        }
+    }
+}
---
+++ ReviewsSite/Models/VideoGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Models
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ReleaseDate { get; set; }
+        [NotMapped]
+        public double AverageRating { get; set; }
+        public virtual List<Review> Reviews { get; set; }
+        public virtual List<VideoGamePlatform> Platforms {get; set;}
+        public string Description { get; set; }
+        public string PictureURL { get; set; }
+        public string VideoURL { get; set; }
+        //Eventually add a picture(virtual)
+        // by url add a string feild
+
+        public void GetAverage()
+        {
+            if(Reviews.Count > 0)
+            {
+                AverageRating = Reviews.Select(r => r.Rating).Average();
+            }
+            else
+            {
+                AverageRating = 0;
+            }
+        }
+    }
+
+
+
+}
