b058d09ec421efe99dba73cc22c6d6f2f1b07c83
Jesse Feller <51204244+jfeller403@users.noreply.github.com>
2021-10-28 21:00:35 -0400

    Initialize wwwroot-folder

---
+++ ReviewsSite/Controllers/VideoGameController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReviewsSite.Models;
+using ReviewsSite.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Controllers
+{
+    public class VideoGameController : Controller
+    {
+        public VideoGameRepository videoGameRepo;
+        public VideoGameController(VideoGameRepository videoGameRepo)
+        {
+            this.videoGameRepo = videoGameRepo;
+        }
+        public ViewResult Index()
+        {
+            return View(videoGameRepo.GetAll());
+        }
+        public ViewResult Details(int id)
+        {
+            return View(videoGameRepo.GetByID(id));
+        }
+        public IActionResult Create()
+        {
+            return View(new VideoGame());
+        }
+    }
+}
