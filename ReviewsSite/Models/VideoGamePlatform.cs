08881a5f80ed3cad2f3ee0a896df75579142229f
Jesse Feller <51204244+jfeller403@users.noreply.github.com>
2021-10-28 21:00:40 -0400

    Initialize Platforms

---
+++ ReviewsSite/Models/VideoGamePlatform.cs
@@ -0,0 +1,16 @@
+﻿using ReviewsSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite
+{
+    public class VideoGamePlatform : BaseModel
+    {
+        public int VideoGameId { get; set; }
+        public virtual VideoGame VideoGame { get; set; }
+        public int PlatformId { get; set; }
+        public virtual Platform Platform { get; set; }
+    }
+}
