674a50b85748082c6d3e4dde25e1de990d21b235
Jesse Feller <51204244+jfeller403@users.noreply.github.com>
2021-10-28 21:00:33 -0400

    Initialize Models1

---
+++ ReviewsSite/Models/Platform.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Models
+{
+    public class Platform
+    {
+        public int Id { get; set; }
+
+
+    }
+}
---
+++ ReviewsSite/Models/VideoGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Models
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Rating { get; set; }
+        public string ReleaseDate { get; set; }
+        public virtual List<Platform> Platforms { get; set; }
+        public string Description { get; set; }
+    }
+
+   
+
+
+}
