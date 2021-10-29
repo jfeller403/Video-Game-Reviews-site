fd83ebbb2e02e3046ac007806970adee9c75844a
Jesse Feller <51204244+jfeller403@users.noreply.github.com>
2021-10-28 21:00:37 -0400

    Initialize layout-cont

---
+++ ReviewsSite/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReviewsSite.Models;
+using ReviewsSite.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Properties.Controllers
+{
+    public class HomeController : Controller
+    {
+        private VideoGameContext db;
+        public VideoGameRepository videoGameRepo;
+        public HomeController (VideoGameContext db)
+        {
+            this.db = db;
+        }
+
+    public IActionResult Index()
+    {
+        return View(db.VideoGames.ToList());
+    }
+
+    public IActionResult Create()
+    {
+        return View(new VideoGame());
+    }
+    
+    [HttpPost]
+    public ActionResult Create(VideoGame game)
+    {
+        videoGameRepo.Create(game);
+        return RedirectToAction("Index");
+    }
+    
+    }
+}
