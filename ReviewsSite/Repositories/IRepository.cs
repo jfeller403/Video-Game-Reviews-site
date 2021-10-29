2b424f6450485752a988833b4eb9e5078f6a959f
Jesse Feller <51204244+jfeller403@users.noreply.github.com>
2021-10-28 21:00:37 -0400

    Initialize wwwroot-folder2

---
+++ ReviewsSite/Repositories/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Repositories
+{
+    public interface IRepository<T>
+    {
+        //c
+        void Create(T obj);
+
+        //r
+        IEnumerable<T> GetAll();
+        T GetByID(int id);
+        //u
+        void Update(T obj);
+        //d 
+        void Delete(T obj);
+
+    }
+}
