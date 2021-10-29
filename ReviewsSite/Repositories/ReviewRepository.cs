ef855fa2f105f1f1004874f8a25b2b04b5e8467c
Jesse Feller <51204244+jfeller403@users.noreply.github.com>
2021-10-28 21:00:34 -0400

    Initialize details-view-feature-TWO

---
+++ ReviewsSite/Repositories/ReviewRepository.cs
@@ -0,0 +1,44 @@
+﻿using ReviewsSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Repositories
+{
+    public class ReviewRepository : IRepository<Review>
+    {
+        public VideoGameContext db;
+        
+        public ReviewRepository(VideoGameContext db)
+        {
+            this.db = db;
+        }
+        
+
+        public void Create(Review obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Review obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Review GetByID(int id)
+        {
+            return db.Reviews.Where(r => r.Id == id).FirstOrDefault();
+        }
+
+        public IEnumerable<Review> GetAll()
+        {
+            return db.Reviews.ToList();
+        }
+
+        public void Update(Review obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
