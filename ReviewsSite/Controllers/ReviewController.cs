29c343d573a8adfe28c1f3df68e6cc3c3b060d0b
Jesse Feller <51204244+jfeller403@users.noreply.github.com>
2021-10-28 21:00:39 -0400

    Initialize GenericRepos

---
+++ ReviewsSite/Controllers/ReviewController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReviewsSite.Models;
+using ReviewsSite.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Controllers
+{
+    public class ReviewController : Controller
+    {
+        public ReviewRepository reviewRepo;
+
+        public ReviewController(ReviewRepository reviewRepo)
+        {
+            this.reviewRepo = reviewRepo;
+        }
+        public ViewResult Index(int? id)
+        {
+            if(id == null)
+            {
+                return View(reviewRepo.GetAllOrdered());
+            } 
+            else
+            {
+                return View(reviewRepo.GetByGameId((int)id));
+            }
+            
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ ReviewsSite/Models/Platform.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Models
+{
+    public class Platform : BaseModel
+    {
+        public string Name { get; set; }
+
+        public virtual List<VideoGamePlatform> VideoGames { get; set; }
+    }
+}
